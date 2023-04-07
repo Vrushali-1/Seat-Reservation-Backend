df4ec93c26c1e3c44ab591f74b6bdb41492eac91
Vrushali Vishal Patil <vrushali@Vrushalis-MacBook-Air.local>
2023-04-06 23:11:31 -0400

    error removed

--- nosqldatabase.js
+++ nosqldatabase.js
@@ -3,6 +3,6 @@ const mongoose = require("mongoose");
 mongoose.connect(`mongodb+srv://seat-reservation:UMBC12345678@cluster1.rxfebt6.mongodb.net/seat-reservation?retryWrites=true&w=majority`,
 { useNewUrlParser: true , 
     useUnifiedTopology: true  })
-.then(()=>{console.log("connected to mongodb database database")});
+.then(()=>{console.log("connected to mongodb database")});
 
 module.exports = mongoose
